38d6c47752f4b8ee1e96f1c6fdfadbb86063c930
Adam Brown <abrown0596@hotmail.co.uk>
2022-01-24 12:13:09 +0000

    string and number test

--- Test.py
+++ Test.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+firstname = "Adam"
+lastname = "Brown"
+print("Hello" + " " + firstname + " " + lastname +"!")
+
+print("")
+
+number1 = int(input("Please enter the first number: "))
+number2 = int(input("Please enter the second number: "))
+answer = number1 + number2
+
+print(number1, "+", number2, "=", answer)
